50a6aab2b7d6d12e54a24f5d5ee012bd6f7003f7
sklores <sklores@gmail.com>
2025-09-14 23:48:15 -0400

    refactor: centralize styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global.css'
+import './styles/topbar.css'
+import './styles/kpi.css'
+import './styles/bottom.css'
+import './styles/marquee.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
